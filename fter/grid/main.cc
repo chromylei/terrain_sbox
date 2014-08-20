8fbea09bea9a894fc9bcd17cbb3a38ab05d9f633
lenxyang <lenxyang@gmail.com>
2014-08-20 16:47:51 +0800

    add grid

---
+++ fter/grid/main.cc
@@ -0,0 +1,115 @@
+#include "azer/render/render.h"
+#include "azer/math/math.h"
+#include "azer/util/util.h"
+#include "base/base.h"
+#include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "tersbox/fter/base/camera_control.h"
+
+#include <tchar.h>
+
+#include "diffuse.afx.h"
+#define EFFECT_GEN_DIR "out/dbg/gen/tersbox/fter/grid/"
+#define SHADER_NAME "diffuse.afx"
+const char* kHeightmapPath = "sbox/terrain/res/heightmap01.bmp";
+using base::FilePath;
+
+class MainDelegate : public azer::WindowHost::Delegate {
+ public:
+  MainDelegate() : tile_(256) {
+  }
+  virtual void OnCreate() {}
+
+  void Init();
+  virtual void OnUpdateScene(double time, float delta_time);
+  virtual void OnRenderScene(double time, float delta_time);
+  virtual void OnQuit() {}
+ private:
+  void InitPhysicsBuffer(azer::RenderSystem* rs);
+  azer::Camera camera_;
+  azer::util::Tile tile_;
+  azer::VertexBufferPtr vb_;
+  azer::IndicesBufferPtr ib_;
+  std::unique_ptr<DiffuseEffect> effect_;
+
+  DISALLOW_COPY_AND_ASSIGN(MainDelegate);
+};
+
+void MainDelegate::Init() {
+  azer::RenderSystem* rs = azer::RenderSystem::Current();
+  azer::Renderer* renderer = rs->GetDefaultRenderer();
+  renderer->SetViewport(azer::Renderer::Viewport(0, 0, 800, 600));
+  CHECK(renderer->GetFrontFace() == azer::kCounterClockwise);
+  CHECK(renderer->GetCullingMode() == azer::kCullBack);
+  renderer->SetFillMode(azer::kWireFrame);
+  renderer->EnableDepthTest(true);
+  camera_.SetPosition(azer::Vector3(0.0f, 0.0f, 5.0f));
+  tile_.Init();
+
+  azer::ShaderArray shaders;
+  CHECK(azer::LoadVertexShader(EFFECT_GEN_DIR SHADER_NAME ".vs", &shaders));
+  CHECK(azer::LoadPixelShader(EFFECT_GEN_DIR SHADER_NAME ".ps", &shaders));
+  effect_.reset(new DiffuseEffect(shaders.GetShaderVec(), rs));
+  InitPhysicsBuffer(rs);
+}
+
+
+void MainDelegate::InitPhysicsBuffer(azer::RenderSystem* rs) {
+  azer::VertexDataPtr vdata(
+      new azer::VertexData(effect_->GetVertexDesc(), tile_.GetVertexNum()));
+  DiffuseEffect::Vertex* vertex = (DiffuseEffect::Vertex*)vdata->pointer();
+  int cnt = 0;
+  for (int i = 0; i < tile_.GetCellNum(); ++i) {
+    for (int j = 0; j < tile_.GetCellNum(); ++j) {
+      const azer::Vector3& pos = tile_.vertices()[cnt++];
+      DiffuseEffect::Vertex* v = vertex + cnt;
+      v->position = azer::Vector4(pos, 1.0f) * 0.05f;
+      v++;
+    }
+  }
+
+  azer::IndicesDataPtr idata_ptr(
+      new azer::IndicesData(tile_.indices().size(), azer::IndicesData::kUint32,
+                            azer::IndicesData::kMainMemory));
+  memcpy(idata_ptr->pointer(), &(tile_.indices()[0]),
+         sizeof(int32) * tile_.indices().size());
+
+  vb_.reset(rs->CreateVertexBuffer(azer::VertexBuffer::Options(), vdata));
+  ib_.reset(rs->CreateIndicesBuffer(azer::IndicesBuffer::Options(), idata_ptr));
+}
+
+
+void MainDelegate::OnUpdateScene(double time, float delta_time) {
+  float rspeed = 3.14f * 2.0f / 4.0f;
+  azer::Radians camera_speed(azer::kPI / 2.0f);
+  UpdatedownCamera(&camera_, camera_speed, delta_time);
+}
+
+void MainDelegate::OnRenderScene(double time, float delta_time) {
+  azer::RenderSystem* rs = azer::RenderSystem::Current();
+  azer::Renderer* renderer = rs->GetDefaultRenderer();
+  renderer->SetCullingMode(azer::kCullNone);
+  DCHECK(NULL != rs);
+  renderer->Clear(azer::Vector4(0.0f, 0.0f, 0.0f, 1.0f));
+  renderer->ClearDepthAndStencil();
+
+  azer::Matrix4 world = std::move(azer::Translate(0.0f, 0.0f, 0.0f));
+  effect_->SetPVW(std::move(camera_.GetProjViewMatrix() * world));
+  effect_->Use(renderer);
+  renderer->DrawIndex(vb_.get(), ib_.get(), azer::kTriangleList);
+}
+
+int main(int argc, char* argv[]) {
+  ::base::InitApp(&argc, &argv, "");
+  
+  MainDelegate delegate;
+  azer::WindowHost win(azer::WindowHost::Options(), &delegate);
+  win.Init();
+  CHECK(azer::LoadRenderSystem(&win));
+  LOG(ERROR) << "Current RenderSystem: " << azer::RenderSystem::Current()->name();
+  delegate.Init();
+  win.Show();
+  azer::MainRenderLoop(&win);
+  return 0;
+}
+
