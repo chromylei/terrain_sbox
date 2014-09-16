65ccf52985a5682fb01a112a16dc1a400a701007
lenxyang <lenxyang@gmail.com>
2014-09-16 17:21:54 +0800

    .

--- tertur/bitmapclouds/skyplane.h
+++ tertur/bitmapclouds/skyplane.h
@@ -2,16 +2,23 @@
 
 #include "azer/render/render.h"
 #include "base/basictypes.h"
+#include "azer/util/util.h"
 
 #include "skyplane.afx.h"
 
 class SkyPlane {
  public:
-  SkyPlane() {}
+  SkyPlane() : tile_(9, 8.0f) {}
   bool Init(azer::RenderSystem* rs);
   void Render(const azer::Camera& camera, azer::Renderer* renderer);
  private:
+  azer::Tile tile_;
   azer::VertexBufferPtr vb_;
+  azer::IndicesBufferPtr ib_;
   std::unique_ptr<SkyPlaneEffect> effect_;
+  azer::TexturePtr tex_;
+
+  static const float kSkyPlaneWidth;
+  static const float kSkyPlaneResolution;
   DISALLOW_COPY_AND_ASSIGN(SkyPlane);
 };
